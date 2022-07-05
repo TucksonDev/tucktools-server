913c4ca01edea421d002334de654034f34716d57
TucksonDev <tucksondev@gmail.com>
2022-07-05 13:46:58 +0100

    Added CORS filter to allow only requests from specific domains

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,39 @@
 import { NestFactory } from '@nestjs/core';
-import { ValidationPipe } from '@nestjs/common';
+import { UnauthorizedException, ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+
+  // CORS
+  const allowedURLs : string = configService.get('CORS_ALLOWED_URLS');
+  app.enableCors({
+    // TODO: For some reason, I can't specify the allowed origins like this,
+    // instead I have to do it with a function
+    // origin: allowedURLs.split(","),
+    origin: function(origin, callback) {
+      if (!allowedURLs) {
+        // console.log('ALLOWED URLS env option not set');
+        callback(null, true);
+        return;
+      }
+
+      if (allowedURLs.split(",").indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new UnauthorizedException(origin + ' is not allowed by CORS'));
+      }
+    }
+  });
+
+  // Validation
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true
   }));
+
+  // Start listening
   await app.listen(3030);
 }
 bootstrap();
