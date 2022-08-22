6ba3952e6d9291f80ab62ed8cc609533ecfcc980
TucksonDev <tucksondev@gmail.com>
2022-08-22 22:37:40 +0100

    Simplify deployment files to test

--- .platform/hooks/postdeploy/01_install_ipfs.sh
+++ .platform/hooks/postdeploy/01_install_ipfs.sh
@@ -2,15 +2,15 @@
 
 # Move to the ipfs directory
 mkdir ~/ipfs-daemon
-cd ~/ipfs-daemon
+# cd ~/ipfs-daemon
 
 # Download and unzip source code
-wget https://dist.ipfs.tech/kubo/v0.14.0/kubo_v0.14.0_linux-amd64.tar.gz
-tar -xvzf kubo_v0.14.0_linux-amd64.tar.gz
+# wget https://dist.ipfs.tech/kubo/v0.14.0/kubo_v0.14.0_linux-amd64.tar.gz
+# tar -xvzf kubo_v0.14.0_linux-amd64.tar.gz
 
 # Install source code
-cd kubo
-sudo bash install.sh
+# cd kubo
+# sudo bash install.sh
 
 # Initialize ipfs
-ipfs init --profile server
+# ipfs init --profile server
--- .platform/hooks/postdeploy/02_run_ipfs.sh
+++ .platform/hooks/postdeploy/02_run_ipfs.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Run IPFS daemon
-ipfs daemon
+# ipfs daemon &
